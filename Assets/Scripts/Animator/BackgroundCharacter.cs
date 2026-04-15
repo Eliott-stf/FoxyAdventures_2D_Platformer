d87c383968be993882e5b623b62df77b4e5d7ea1
ES_PREPA_DEV <setifeliott@gmail.com>
2026-04-15 15:35:44 +0200

    edit: Menu transition on scene

--- Assets/Scripts/Animator/BackgroundCharacter.cs
+++ Assets/Scripts/Animator/BackgroundCharacter.cs
@@ -1,75 +1,106 @@
+using Player;
+
 namespace Animator
 {
     using UnityEngine;
+    using Menu; // Requis pour communiquer avec le menu
 
     public class BackgroundCharacter : MonoBehaviour
     {
-        private Animator animator;
+        private UnityEngine.Animator animator;
         private ParticleSystem dustParticle;
-        public float runSpeed = 10f;
-        public float startX = 1.3f; // Point A (entrée)
-        public float stopX = 35f; // Milieu, où il stoppe
-        public float exitX = 70.5f; // Point B (sortie)
-        public float idleDuration = 2f;
 
+        [Header("Mouvement")]
+        public float runSpeed = 10f;
+        public float startX = 1.3f;
+        public float stopX = 35f;
+        public float exitX = 70.5f;
+
+        [Header("Références")]
+        [SerializeField] private PlayerController playerController;
+        [SerializeField] private MainMenuButton mainMenu;
+
+        [Header("HUD")]
+        public GameObject tuto;
+        public GameObject lifeDisplay;
+        public GameObject achivements;
+        
         private enum State
         {
-            Running,
-            Idle,
-            RunningOut
+            RunningIn,
+            Waiting,
+            RunningOut,
+            Finished
         }
 
-        private State state = State.Running;
-        private float idleTimer = 0f;
+        private State state = State.RunningIn;
 
         void Start()
         {
-            animator = GetComponentInChildren<Animator>();
+            animator = GetComponentInChildren<UnityEngine.Animator>();
             dustParticle = GetComponentInChildren<ParticleSystem>();
             transform.position = new Vector2(startX, transform.position.y);
+
+            if (playerController != null)
+            {
+                playerController.enabled = false;
+            }
         }
 
         void Update()
         {
             switch (state)
             {
-                case State.Running:
+                case State.RunningIn:
                     transform.Translate(Vector2.right * runSpeed * Time.deltaTime);
                     animator.SetBool("isRunning", true);
-                    dustParticle.Play();
+                    if (!dustParticle.isPlaying) dustParticle.Play();
 
                     if (transform.position.x >= stopX)
                     {
                         transform.position = new Vector2(stopX, transform.position.y);
-                        state = State.Idle;
+                        state = State.Waiting;
                         animator.SetBool("isRunning", false);
-                        dustParticle.Stop(); 
-                    }
+                        dustParticle.Stop();
 
+                        // Déclenche l'apparition du menu
+                        if (mainMenu != null)
+                        {
+                            mainMenu.ShowMenu();
+                        }
+                    }
                     break;
 
-                case State.Idle:
-                    idleTimer += Time.deltaTime;
-                    if (idleTimer >= idleDuration)
-                    {
-                        idleTimer = 0f;
-                        state = State.RunningOut;
-                        animator.SetBool("isRunning", true);
-                    }
-
+                case State.Waiting:
                     break;
 
                 case State.RunningOut:
                     transform.Translate(Vector2.right * runSpeed * Time.deltaTime);
-                    dustParticle.Play();
+                    animator.SetBool("isRunning", true);
+                    if (!dustParticle.isPlaying) dustParticle.Play();
 
                     if (transform.position.x >= exitX)
                     {
-                        gameObject.SetActive(false);
+                        if (playerController != null)
+                        {
+                            playerController.enabled = true;
+                        }
+                        state = State.Finished;
+                        animator.SetBool("isRunning", false);
+                        dustParticle.Stop();
+                        
+                        tuto.SetActive(true);
+                        lifeDisplay.SetActive(true);
+                        achivements.SetActive(true);
                     }
-
                     break;
             }
+        }
+
+        // Méthode appelée par le script du menu
+        public void TriggerExitAnimation()
+        {
+            state = State.RunningOut;
         }
     }
 }
--- Assets/Scripts/Menu/MainMenuButtons.cs
+++ Assets/Scripts/Menu/MainMenuButtons.cs
@@ -1,39 +1,88 @@
 namespace Menu
 {
+    using System.Collections;
     using UnityEngine;
-    using UnityEngine.SceneManagement;
     using UnityEngine.EventSystems;
     using TMPro;
+    using Manager;
+    using Animator; // Requis pour communiquer avec le personnage
 
-    public class MainMenuButtons : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    public class MainMenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
-        private TMP_Text tmpText;
+        [Header("Interface")]
+        [SerializeField] private CanvasGroup menuCanvasGroup;
+        [SerializeField] private float fadeDuration = 1f;
+
+        [Header("Bouton Visuel")]
         public Color normalColor = Color.white;
-        public Color hoverColor = new Color(0.69f, 0.66f, 0.66f); 
+        public Color hoverColor = new Color(0.69f, 0.66f, 0.66f);
+        private TMP_Text tmpText;
+
+        [Header("Références Externes")]
+        [SerializeField] private BackgroundCharacter backgroundCharacter;
 
         void Start()
         {
             tmpText = GetComponentInChildren<TMP_Text>();
+            
+            if (menuCanvasGroup != null)
+            {
+                menuCanvasGroup.alpha = 0f;
+            }
         }
 
         public void OnPointerEnter(PointerEventData eventData)
         {
-            tmpText.color = hoverColor;
+            if (tmpText != null) tmpText.color = hoverColor;
         }
 
         public void OnPointerExit(PointerEventData eventData)
         {
-            tmpText.color = normalColor;
+            if (tmpText != null) tmpText.color = normalColor;
+        }
+
+        // Méthode appelée par le BackgroundCharacter quand il s'arrête
+        public void ShowMenu()
+        {
+            StartCoroutine(FadeMenu(0f, 1f));
         }
 
         public void PlayGame()
         {
-            SceneManager.LoadScene("LevelDay");
+            StartCoroutine(StartGameSequence());
         }
 
         public void QuitGame()
         {
             Application.Quit();
+        }
+
+        private IEnumerator StartGameSequence()
+        {
+            // 1. Disparition du menu
+            yield return StartCoroutine(FadeMenu(1f, 0f));
+            menuCanvasGroup.gameObject.SetActive(false);
+
+            // 2. Déclenchement de la course du personnage
+            if (backgroundCharacter != null)
+            {
+                backgroundCharacter.TriggerExitAnimation();
+            }
+        }
+
+        private IEnumerator FadeMenu(float from, float to)
+        {
+            float elapsed = 0f;
+            menuCanvasGroup.alpha = from;
+
+            while (elapsed < fadeDuration)
+            {
+                elapsed += Time.deltaTime;
+                menuCanvasGroup.alpha = Mathf.Lerp(from, to, elapsed / fadeDuration);
+                yield return null;
+            }
+
+            menuCanvasGroup.alpha = to;
         }
     }
 }
